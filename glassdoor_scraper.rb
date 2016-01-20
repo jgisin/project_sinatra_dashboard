db654d06dd595f6b6a0314191ea244ab103d4ed6
Jeffrey Gisin <jgisin@gmail.com>
2016-01-20 15:50:41 -0700

    no more crap gem

--- glassdoor_scraper.rb
+++ glassdoor_scraper.rb
@@ -1,10 +1,12 @@
 require 'httparty'
-require 'glassdoor-api'
+require 'pry-byebug'
+require 'figaro'
 require 'pp'
 
 Figaro.application = Figaro::Application.new(
   environment: "development",
-  path: "config/application.yml")
+  path: "config/application.yml"
+  )
 Figaro.load
 
 
@@ -13,10 +15,15 @@ class GlassdoorAPI
 
   ID = Figaro.env.GLASSDOOR_ID
   KEY = Figaro.env.GLASSDOOR_KEY
+  IP = Figaro.env.USER_IP
+  BASE_URI = 'http://api.glassdoor.com/api/api.htm?'
 
   def initialize(location = "reno,us", days = 5)
-     @options = { :query => {:q => location, :cnt => days, :APPID => ENV["APPID"]}}
-     @gdapi = Glassdoor.new
+     @options = { :query => {'v' => 1, 'format' => 'json', 't.p' => ID, 't.k' => KEY, 'userip' => IP, 'action' => 'employers'}}
+
   end
 
 end
+
+
+gd = GlassdoorAPI.new
